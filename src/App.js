cb4deb5eb596bc3ee1f598445ced28b2119db5ca
Bella Santiago <bellasantiago.adl@gmail.com>
2021-05-10 21:24:43 +0930

    Added card swipe feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Navbar from './components/Navbar'
+import Cards from './components/Cards'
 
 function App() {
   return (
     <div className="app">
       <Navbar />
+      <br></br>
+      <Cards />
       {/* Navbar */}
       {/* Cards */}
       {/* Navigation */}
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import { React, useState } from 'react';
+import TinderCard from 'react-tinder-card';
 
-function Cards() {
+const db = [
+    {
+      name: 'The Matrix',
+      url: 'img/sample.jpeg'
+    }
+  ]
+  
+  function Cards () {
+    const characters = db
+    const [lastDirection, setLastDirection] = useState()
+  
+    const swiped = (direction, nameToDelete) => {
+      console.log('removing: ' + nameToDelete)
+      setLastDirection(direction)
+    }
+  
+    const outOfFrame = (name) => {
+      console.log(name + ' left the screen!')
+    }
+  
     return (
-        <div>
-            
+      <div>
+        <h3 className='infoText'>Swipe RIGHT or LEFT to choose your movie!</h3>
+
+        <div className='cardContainer'>
+        <br></br>
+          {characters.map((character) =>
+            <TinderCard className='swipe' key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
+              <div style={{ backgroundImage: 'url(' + character.url + ')' }} className='card'>
+                <h3>{character.name}</h3>
+              </div>
+            </TinderCard>
+          )}
         </div>
+        {lastDirection ? <h2 className='infoText'>You swiped {lastDirection}</h2> : <h2 className='infoText' />}
+      </div>
     )
-}
+  }
 
 export default Cards
