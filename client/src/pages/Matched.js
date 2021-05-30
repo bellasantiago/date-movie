1bf43022f0e48124c24fd831883c482a60b066e9
Bella Santiago <bellasantiago.adl@gmail.com>
2021-05-30 13:08:43 +0930

    Matched page working

--- client/src/pages/Matched.js
+++ client/src/pages/Matched.js
@@ -8,6 +8,10 @@ function Matched() {
     const { userId } = useContext(MyContext);
     // const [matchedMovies, setMatchedMovies] = useState([])
     const [currentUserMovies, setCurrentUserMovies] = useState([]);
+    const [checkUser, setCheckUser] = useState();
+    const [allUsers, setAllUsers] = useState([]);
+    const [checkUserMovies, setCheckUserMovies] = useState([]);
+    const [matches, setMatches] = useState([]);
 
     // Load User data
     useEffect(() => {
@@ -19,10 +23,48 @@ function Matched() {
             .catch(err => console.log(err));
         API.getUsers()
             .then(res => {
-                console.log(res.data)
+                // console.log(res.data)
+                setAllUsers(res.data)
+                console.log(allUsers)
             })
             .catch(err => console.log(err));
     }, [])
+
+    const handleFormSubmit = event => {
+        event.preventDefault();
+        console.log(event.target);
+        let formData = new FormData(event.target);
+
+        let body = {};
+
+        for (let pair of formData.entries()) {
+            body[pair[0]] = pair[1]
+        }
+        setCheckUser(JSON.stringify(body.checkUser))
+        console.log(checkUser)
+
+        var userData = allUsers.filter(function(user) {
+            return user.username === "BellaSantiago"
+        })
+        console.log(userData.[0].movies)
+        setCheckUserMovies(userData.[0].movies)
+
+        var matches = [];
+        function getMatch(a, b) {
+        
+            for ( var i = 0; i < a.length; i++ ) {
+                for ( var e = 0; e < b.length; e++ ) {
+                    if ( a[i] === b[e] ) matches.push( a[i] );
+                }
+            }
+            return matches;
+        }
+        
+        getMatch(currentUserMovies, checkUserMovies);
+        console.log(matches)
+        setMatches(matches)
+
+    }
 
     return (
         <div>
@@ -32,7 +74,8 @@ function Matched() {
             <br></br>
             <br></br>
 
-            <div className="form-label-group">
+            <form className="form-label-group"
+            onSubmit={handleFormSubmit}>
                 <input
                     type="text"
                     id="checkUser"
@@ -45,17 +88,17 @@ function Matched() {
                     className="btn btn-sm btn-primary btn-block text-uppercase"
                     type="submit"
                 >Send</button>
-            </div>
+            </form>
 
             <div className="jumbotron jumbotron-fluid" id="matchedJumbo">
                 <table className="table">
                     <thead>
                         <tr>
-                            <th scope="col">MATCHED MOVIES WITH SAMPLE USER</th>
+                            <th scope="col">Matched Movies with user {checkUser}</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {currentUserMovies.map(movie => {
+                        {matches.map(movie => {
                             return (
                                 <tr key={movie}>
                                     <td>{movie}</td>
@@ -65,7 +108,6 @@ function Matched() {
                     </tbody>
                 </table>
             </div>
-            <br></br>
             <Link to="/Movies">
                 <button
                     className="btn btn-sm btn-primary btn-block text-uppercase"
